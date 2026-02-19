537d530922c8b36b18ea663274e876f1c654737b
Graham Beckley <gbeckley@mozilla.com>
2026-02-19 11:23:43 -0500

    upgrade lazer_app to Angular 21
    Used ng update @angular/core@21 @angular/cli@21
    Manual changes required:
    - .browserslistrc: raised minimums to Angular 21's supported baseline

--- lazer_app/projectLazer/src/main.ts
+++ lazer_app/projectLazer/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -5,7 +6,7 @@ import { AppModule } from './app/app.module';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch((err) => console.log(err));
 
 defineCustomElements(window);
