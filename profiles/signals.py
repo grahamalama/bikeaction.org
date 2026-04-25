b670801bfaa1f394f85f12fb1bb2b110282e66b4
Ee Durbin <ewdurbin@gmail.com>
2026-04-25 12:23:14 -0400

    fix signal for adding/removing organizer group

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,15 +1,12 @@
 from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.models import Group
-from django.db.models.signals import m2m_changed, post_delete, post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from facets.models import District
-from profiles.models import Profile
 from profiles.tasks import add_user_to_connected_role, remove_user_from_connected_role
 
 ORGANIZER_GROUP_NAME = "Organizers"
-
-_pending_clear_profiles: dict[int, list] = {}
 
 
 @receiver(post_save, sender=SocialAccount, dispatch_uid="social_account_post_save")
@@ -33,27 +30,18 @@ def _sync_organizer_group(profile):
 
 
 @receiver(
-    m2m_changed,
+    post_save,
     sender=District.organizers.through,
-    dispatch_uid="district_organizers_changed",
+    dispatch_uid="district_organizers_through_post_save",
 )
-def district_organizers_changed(sender, instance, action, reverse, pk_set, **kwargs):
-    if action == "pre_clear":
-        if reverse:
-            _pending_clear_profiles[id(instance)] = [instance]
-        else:
-            _pending_clear_profiles[id(instance)] = list(instance.organizers.all())
-        return
+def district_organizers_through_post_save(sender, instance, **kwargs):
+    _sync_organizer_group(instance.profile)
 
-    if action not in {"post_add", "post_remove", "post_clear"}:
-        return
 
-    if action == "post_clear":
-        profiles = _pending_clear_profiles.pop(id(instance), [])
-    elif reverse:
-        profiles = [instance]
-    else:
-        profiles = Profile.objects.filter(pk__in=pk_set or [])
-
-    for profile in profiles:
-        _sync_organizer_group(profile)
+@receiver(
+    post_delete,
+    sender=District.organizers.through,
+    dispatch_uid="district_organizers_through_post_delete",
+)
+def district_organizers_through_post_delete(sender, instance, **kwargs):
+    _sync_organizer_group(instance.profile)
